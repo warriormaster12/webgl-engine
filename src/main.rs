409706a8c609781bab9a25c9d036c53c9617ee57
warriormaster12 <streng.alexander@outlook.com>
2023-06-29 21:36:19 +0300

    pink cube

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{borrow::Cow, mem};
-use cgmath::Vector4;
+use std::{borrow::Cow, mem, f32::consts};
+use wgpu::util::DeviceExt;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -14,6 +14,74 @@ use bytemuck::{Pod, Zeroable};
 struct TriangleUniform {
     color: [f32; 4]
 }
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct Vertex {
+    pos: [f32; 4],
+    tex_coord: [f32; 2]
+}
+
+fn vertex(pos: [i8; 3], tex_coords: [i8; 2]) -> Vertex {
+    Vertex { pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0], tex_coord: [tex_coords[0] as f32, tex_coords[1] as f32] }
+}
+
+fn create_vertices() -> (Vec<Vertex>, Vec<u16>) {
+    let vertex_data = [
+        // top (0, 0, 1)
+        vertex([-1, -1, 1], [0, 0]),
+        vertex([1, -1, 1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([-1, 1, 1], [0, 1]),
+        // bottom (0, 0, -1)
+        vertex([-1, 1, -1], [1, 0]),
+        vertex([1, 1, -1], [0, 0]),
+        vertex([1, -1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        vertex([1, -1, -1], [0, 0]),
+        vertex([1, 1, -1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([1, -1, 1], [0, 1]),
+        // left (-1, 0, 0)
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, 1, 1], [0, 0]),
+        vertex([-1, 1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        vertex([1, 1, -1], [1, 0]),
+        vertex([-1, 1, -1], [0, 0]),
+        vertex([-1, 1, 1], [0, 1]),
+        vertex([1, 1, 1], [1, 1]),
+        // back (0, -1, 0)
+        vertex([1, -1, 1], [0, 0]),
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, -1, -1], [1, 1]),
+        vertex([1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0, 1, 2, 2, 3, 0, // top
+        4, 5, 6, 6, 7, 4, // bottom
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
+    (vertex_data.to_vec(), index_data.to_vec())
+}
+
+fn generate_matrix(aspect_ratio: f32) -> glam::Mat4 {
+    let projection = glam::Mat4::perspective_rh(consts::FRAC_PI_4, aspect_ratio, 1.0, 10.0);
+    let view = glam::Mat4::look_at_rh(
+        glam::Vec3::new(1.5f32, -5.0, 3.0),
+        glam::Vec3::ZERO,
+        glam::Vec3::Z,
+    );
+    projection * view
+}
+
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
     let size = window.inner_size();
@@ -51,12 +119,45 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         format: swapchain_format,
         width: size.width,
         height: size.height,
-        present_mode: wgpu::PresentMode::Immediate,
+        present_mode: wgpu::PresentMode::Mailbox,
         alpha_mode: swapchain_capabilities.alpha_modes[0],
         view_formats: vec![],
     };
 
     surface.configure(&device, &config);
+
+    // Create the vertex and index buffers
+    let vertex_size = mem::size_of::<Vertex>();
+    let (vertex_data, index_data) = create_vertices();
+
+    let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(&vertex_data),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
+
+    let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Index Buffer"),
+        contents: bytemuck::cast_slice(&index_data),
+        usage: wgpu::BufferUsages::INDEX,
+    });
+
+     // Create other resources
+     let mx_total = generate_matrix(config.width as f32 / config.height as f32);
+     let mx_ref: &[f32; 16] = mx_total.as_ref();
+     let matrix_u_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+         label: Some("Uniform Buffer"),
+         contents: bytemuck::cast_slice(mx_ref),
+         usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+     });
+
+    //Color uniform buffer
+    let uniform_buf = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("Uniform Buffer Color"),
+        size: mem::size_of::<TriangleUniform>() as u64,
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
 
     //Shader and pipeline
     let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor { 
@@ -70,6 +171,16 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         entries: &[
             wgpu::BindGroupLayoutEntry {
                 binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: wgpu::BufferSize::new(64),
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
                 visibility: wgpu::ShaderStages::FRAGMENT,
                 ty: wgpu::BindingType::Buffer { 
                     ty: wgpu::BufferBindingType::Uniform, 
@@ -86,13 +197,30 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         push_constant_ranges: &[],
     });
 
+    let vertex_buffers = [wgpu::VertexBufferLayout {
+        array_stride: vertex_size as wgpu::BufferAddress,
+        step_mode: wgpu::VertexStepMode::Vertex,
+        attributes: &[
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x4,
+                offset: 0,
+                shader_location: 0,
+            },
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x2,
+                offset: 4 * 4,
+                shader_location: 1,
+            },
+        ],
+    }];
+
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: None,
         layout: Some(&pipeline_layout),
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
-            buffers: &[],
+            buffers: &vertex_buffers,
         },
         fragment: Some(wgpu::FragmentState {
             module: &shader,
@@ -104,24 +232,18 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
     });
-    //Color uniform buffer
-    let uniform_buf = device.create_buffer(&wgpu::BufferDescriptor {
-        label: Some("Uniform Buffer Color"),
-        size: mem::size_of::<Vector4<f32>>() as u64,
-        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
+
     // Vulkan equivalent to descriptor sets
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         layout: &bind_group_layout,
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource:  wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                    buffer: &uniform_buf,
-                    offset: 0,
-                    size: wgpu::BufferSize::new(mem::size_of::<TriangleUniform>() as wgpu::BufferAddress),
-                }),
+                resource: matrix_u_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: uniform_buf.as_entire_binding(),
             },
         ],
         label: None,
@@ -140,6 +262,9 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 config.width = size.width;
                 config.height = size.height;
                 surface.configure(&device, &config);
+                let mx_total = generate_matrix(config.width as f32 / config.height as f32);
+                let mx_ref: &[f32; 16] = mx_total.as_ref();
+                queue.write_buffer(&matrix_u_buffer, 0, bytemuck::cast_slice(mx_ref));
                 // On macos the window needs to be redrawn manually after resizing
                 window.request_redraw();
             }
@@ -171,7 +296,10 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     });
                     rpass.set_pipeline(&render_pipeline);
                     rpass.set_bind_group(0, &bind_group, &[]);
-                    rpass.draw(0..3, 0..1);
+                    rpass.set_bind_group(1, &bind_group, &[]);
+                    rpass.set_index_buffer(index_buf.slice(..), wgpu::IndexFormat::Uint16);
+                    rpass.set_vertex_buffer(0, vertex_buf.slice(..));
+                    rpass.draw_indexed(0..index_data.len() as u32, 0, 0..1);
                 }
                 queue.submit(Some(encoder.finish()));
                 frame.present();
@@ -190,6 +318,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 fn main() {
     let event_loop = EventLoop::new();
     let window = winit::window::Window::new(&event_loop).unwrap();
+    window.set_title("wgpu-engine");
     window.set_inner_size(PhysicalSize::new(1280, 720));
     #[cfg(not(target_arch = "wasm32"))]
     {
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,15 +1,26 @@
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>
+}
+
+@group(0)
+@binding(0)
+var<uniform> transform: mat4x4<f32>;
+
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+fn vs_main(
+    @location(0) position: vec4<f32>,
+    @location(1) tex_coord: vec2<f32>,
+) -> VertexOutput {
+    var result: VertexOutput;
+    result.position = transform * position;
+    return result;
 }
 
 struct TriangleUniform {
     color: vec4<f32>
 }
 @group(0)
-@binding(0)
+@binding(1)
 var<uniform> triangle_data: TriangleUniform;
 
 @fragment
